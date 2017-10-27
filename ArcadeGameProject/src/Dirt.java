d52a5668e678419665b090600dc16db954853680
ladipooo <ladipooo@e0278eb3-f9aa-4575-b39d-65d1bb3aa33f>
2017-10-27 02:24:25 +0000

    Committing Dirt.

---
+++ ArcadeGameProject/src/Dirt.java
@@ -0,0 +1,47 @@
+import java.awt.Shape;
+import java.awt.geom.Point2D;
+
+public class Dirt extends Character{
+
+	public Dirt(DigDugEnvironment world, Point2D point) {
+		super(world, point);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public Shape getShape() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Point2D getCenterPoint() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void updatePosition() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateSize() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateColor() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public double getscore() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
