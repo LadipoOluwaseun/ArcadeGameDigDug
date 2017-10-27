f1da4abb063b86d5527c1cc468b6aaa8d76e96e1
ladipooo <ladipooo@e0278eb3-f9aa-4575-b39d-65d1bb3aa33f>
2017-10-27 01:16:36 +0000

    Made classes and interfaces.

--- ArcadeGameProject/src/DigDugWorld.java
+++ ArcadeGameProject/src/DigDugWorld.java
@@ -1,4 +1,15 @@
 
-public class DigDugWorld {
-
+public class DigDugWorld implements DigDugEnvironment, Drawable, Temporal{
+	boolean isPaused;
+	
+	public DigDugWorld(){
+		
+	}
+	
+	public int lives(){
+		return 0;
+	}
+	public double score(){
+		return 0;
+	}
 }
