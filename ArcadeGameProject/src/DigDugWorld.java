f65258c308c2791645f98805168f1c4314c724c0
ladipooo <ladipooo@e0278eb3-f9aa-4575-b39d-65d1bb3aa33f>
2017-10-27 01:22:34 +0000

    commiting digdugworld.

--- ArcadeGameProject/src/DigDugWorld.java
+++ ArcadeGameProject/src/DigDugWorld.java
@@ -1,3 +1,5 @@
+import java.awt.Shape;
+import java.util.List;
 
 public class DigDugWorld implements DigDugEnvironment, Drawable, Temporal{
 	boolean isPaused;
@@ -11,5 +13,65 @@ public class DigDugWorld implements DigDugEnvironment, Drawable, Temporal{
 	}
 	public double score(){
 		return 0;
+	}
+
+	@Override
+	public void timePassed() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void die() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setIsPaused(boolean isPaused) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean getIsPaused() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Shape getShape() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addStuff(Stuff stuff) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removeStuff(Stuff stuff) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Drawable> getDrawableParts() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void readLevelFile(String filename) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateScore() {
+		// TODO Auto-generated method stub
+		
 	}
 }
