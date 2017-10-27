f4decea245623fc460f080d9f7bfb9cdbfab3473
smithea2 <smithea2@e0278eb3-f9aa-4575-b39d-65d1bb3aa33f>
2017-10-27 01:16:56 +0000

    grabbing drawable

--- ArcadeGameProject/src/SimulationPanel.java
+++ ArcadeGameProject/src/SimulationPanel.java
@@ -1,5 +1,38 @@
+import java.awt.Color;
+
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.JPanel;
 
 public class SimulationPanel extends JPanel{
 
+	
+	private static final int GAP_WIDTH = 10;
+
+	/**
+	 * Constructs a new simulation where the world for the balls has the given
+	 * dimensions and the given background color.
+	 * 
+	 * Note that the actual dimensions of the resulting panel will be larger,
+	 * since the panel also includes the controls for the simulation.
+	 * 
+	 * @param width
+	 * @param height
+	 * @param color
+	 */
+	public SimulationPanel(int width, int height, Color color) {
+		// Change layout so the buttons and the world are side-by-side.
+		setLayout(new BoxLayout(this, BoxLayout.LINE_AXIS));
+		DigDugWorld world = new DigDugWorld(width, height, color);
+		DigDugWorldComponent worldComponent = new DigDugWorldComponent(world);
+		// add "rigid stuff" to force minimum spacing
+		add(Box.createHorizontalStrut(GAP_WIDTH));
+		// add "rigid stuff" to force minimum spacing
+		add(Box.createHorizontalStrut(GAP_WIDTH));
+		add(worldComponent);
+		// adds "stretchy stuff" to force the world to the left
+		add(Box.createGlue());
+		// add "rigid stuff" to force minimum spacing
+		add(Box.createHorizontalStrut(GAP_WIDTH));
+	}
 }
