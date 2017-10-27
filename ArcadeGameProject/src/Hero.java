875705d59fc3629723293839bfdcc0bdaae1dba9
ladipooo <ladipooo@e0278eb3-f9aa-4575-b39d-65d1bb3aa33f>
2017-10-27 02:24:10 +0000

    Commiting Hero.

---
+++ ArcadeGameProject/src/Hero.java
@@ -0,0 +1,50 @@
+import java.awt.Shape;
+import java.awt.geom.Point2D;
+
+public class Hero extends Character{
+
+	public Hero(DigDugEnvironment world, Point2D point) {
+		super(world, point);
+	}
+
+	@Override
+	public Shape getShape() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Point2D getCenterPoint() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void updatePosition() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateSize() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateColor() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public double getscore() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	public void extendCord() {
+		
+	}
+
+}
