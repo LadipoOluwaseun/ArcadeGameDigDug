b54a92ffaad52a02270381b20da7864faf3a2884
calarejf <calarejf@e0278eb3-f9aa-4575-b39d-65d1bb3aa33f>
2017-10-27 01:16:16 +0000

    The start of Main. It runs.

--- ArcadeGameProject/src/Main.java
+++ ArcadeGameProject/src/Main.java
@@ -11,13 +11,33 @@
  * @author Buffalo
  *
  */
+import javax.swing.JFrame;
+
 public class Main {
 
 	/**
+	 * Starts the application.
+	 * 
 	 * @param args
+	 *            ignored
 	 */
 	public static void main(String[] args) {
+		SimulationPanel worlds = constructSimulations();
+		JFrame frame = new DigDugFrame(worlds);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+
+	/**
+	 * This helper method constructs a list of simulation objects that will each
+	 * simulate a world of bouncing balls.
+	 * 
+	 * @return a list of worlds
+	 */
+	private static SimulationPanel constructSimulations() {
+		SimulationPanel sp = new SimulationPanel();
 		
+		return sp;
 	}
 
 }
