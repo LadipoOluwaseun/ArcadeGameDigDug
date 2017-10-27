2f97debfed3908ccebb29dfd4ba647b8064486e0
calarejf <calarejf@e0278eb3-f9aa-4575-b39d-65d1bb3aa33f>
2017-10-27 01:43:35 +0000

    A couple of small comment changes.

--- ArcadeGameProject/src/SimulationPanel.java
+++ ArcadeGameProject/src/SimulationPanel.java
@@ -2,22 +2,21 @@ import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JPanel;
 
+/**
+ * 
+ * Creates a panel to simulate the DigDugWorld.
+ *
+ * @author calarejf.
+ *         Created Oct 26, 2017.
+ */
+
 public class SimulationPanel extends JPanel{
 
 	
-	private static final int GAP_WIDTH = 10;
 
-	/**
-	 * Constructs a new simulation where the world for the balls has the given
-	 * dimensions and the given background color.
-	 * 
-	 * Note that the actual dimensions of the resulting panel will be larger,
-	 * since the panel also includes the controls for the simulation.
-	 * 
-	 * @param width
-	 * @param height
-	 * @param color
-	 */
+	private static final long serialVersionUID = 1L;
+	private static final int GAP_WIDTH = 10;
+	
 	public SimulationPanel() {
 		// Change layout so the buttons and the world are side-by-side.
 		setLayout(new BoxLayout(this, BoxLayout.LINE_AXIS));
