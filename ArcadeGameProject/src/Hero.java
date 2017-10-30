3641ce6ebae206a32e4b914348ed950f1f71112c
calarejf <calarejf@e0278eb3-f9aa-4575-b39d-65d1bb3aa33f>
2017-10-30 03:53:08 +0000

    Finished implementing the cord logic including which way to go.

--- ArcadeGameProject/src/Hero.java
+++ ArcadeGameProject/src/Hero.java
@@ -1,27 +1,24 @@
 import java.awt.Color;
-import java.awt.Dimension;
 import java.awt.geom.Line2D;
 import java.awt.geom.Point2D;
 
 import com.sun.javafx.geom.Rectangle;
 
-
 /**
  * 
  * creates and implements methods of the hero in DigDugWorld.
  *
- * @author smithea2.
- *         Created Oct 26, 2017.
+ * @author smithea2. Created Oct 26, 2017.
  */
 
-public class Hero extends Stuff{
-	
+public class Hero extends Stuff implements Runnable {
+
 	public Point2D.Double point;
-	public int y;
-	public int x;
+	// public int y;
+	// public int x;
 	public Rectangle shape;
 	public Point2D center;
-	private Dimension size;
+	// private Dimension size;
 	private Color color;
 	public int score;
 	public int changeX;
@@ -31,6 +28,8 @@ public class Hero extends Stuff{
 	public boolean cordExtended;
 	public int WIDTH;
 	public int HEIGHT;
+	private int TIME_TO_SLEEP_WHILE_EXTENDING_CORD_IN_MILLISECONDS;
+	char lastDirection;
 
 	public Hero(DigDugEnvironment world, Point2D.Double point) {
 		super(world, point);
@@ -41,33 +40,37 @@ public class Hero extends Stuff{
 		this.color = Color.CYAN;
 		this.WIDTH = 30;
 		this.HEIGHT = 30;
+		this.TIME_TO_SLEEP_WHILE_EXTENDING_CORD_IN_MILLISECONDS = 100;
 		this.shape = new Rectangle((int) point.getX(), (int) point.getY(), this.WIDTH, this.HEIGHT);
-		this.DEFAULT_CORD = new Line2D.Double(new Point2D.Double(this.x, this.y), 
-				new Point2D.Double(this.x+1, this.y+1));
+		this.DEFAULT_CORD = new Line2D.Double(new Point2D.Double(this.point.getX(), this.point.getY()),
+				new Point2D.Double(this.point.getX(), this.point.getY()));
 		this.cord = this.DEFAULT_CORD;
-		System.out.println("i'm a hero!");
+		// System.out.println("i'm a hero!");
 	}
-	
+
 	public void moveUp() {
 		System.out.println("watch me move!");
 		this.changeY = 1;
+		this.lastDirection = 'u';
 	}
-	
+
 	public void moveDown() {
 		System.out.println("watch me move!");
 		this.changeY = -1;
+		this.lastDirection = 'd';
 	}
-	
+
 	public void moveLeft() {
 		System.out.println("watch me move!");
 		this.changeX = -1;
+		this.lastDirection = 'l';
 	}
-	
+
 	public void moveRight() {
 		System.out.println("watch me move!");
 		this.changeX = 1;
+		this.lastDirection = 'r';
 	}
-	
 
 	@Override
 	public Rectangle getShape() {
@@ -80,46 +83,133 @@ public class Hero extends Stuff{
 	}
 
 	public void updatePosition() {
-		this.x = this.x + this.changeX;
-		this.y = this.y + this.changeY;
-		this.point = new Point2D.Double(this.x, this.y);
+		double x = this.point.getX() + this.changeX;
+		double y = this.point.getY() + this.changeY;
+		this.point = new Point2D.Double(x, y);
 	}
 
 	public void updateSize() {
-		this.size = this.size;
+		// this.size = this.size;
 	}
 
 	@Override
 	public double getscore() {
 		return this.score;
 	}
-	
+
 	public Line2D.Double extendCord() {
-		System.out.println("extendCord method in Hero");
-		if(this.cordExtended==false){
-			
-		this.cord = new Line2D.Double(new Point2D.Double(this.x, this.y), 
-				new Point2D.Double(this.x + this.changeX*10, this.y + this.changeY*10));
-		this.cordExtended = true;
+		this.run();
 		return this.cord;
-		}
-		this.cord = this.DEFAULT_CORD;
-		return this.cord;
-		
+
 	}
-//	
-//	public Line2D.Double retractCord() {
-//		this.cord = this.DEFAULT_CORD;
-//		return this.cord;
-//	}
+	//
+	// public Line2D.Double retractCord() {
+	// this.cord = this.DEFAULT_CORD;
+	// return this.cord;
+	// }
 
 	@Override
 	public Color getColor() {
 		return this.color;
 	}
-	
-	public Line2D.Double getCord(){
-//		return this.cord;  
-		return null;}
 
+	public Line2D.Double getCord() {
+		// return this.cord;
+		return null;
+	}
+
+	@Override
+	public void run() {
+		// try {
+		// /*
+		// * Main "brains" inside an infinite loop, so the thread keeps
+		// * running:
+		// */
+		// while (true) {
+		// // Work of the thread can include calls to other methods:
+		// String timeS = getTimeStamp();
+		// System.out.println(timeS + ": " + this.message);
+		//
+		// /*
+		// * After the work is done, put the thread to sleep so other
+		// * threads can work. (Or the CPU can rest if no other threads
+		// * are active.)
+		// */
+		// Thread.sleep(this.pauseInMS);
+		// }
+		// } catch (InterruptedException exception) {
+		// /*
+		// * This Runnable object just exits when interrupted, so there isn't
+		// * anything to do here. Sometimes threads will use _very_ long sleep
+		// * times, with interrupts used to wake them up and do something. That
+		// * is, we don't always exit when interrupted.
+		// */
+		// }
+		try {
+			System.out.println("run method in Hero");
+
+			if (this.lastDirection == 'u') {
+
+				for (int i = 0; i < 30; i++) {
+					this.cord = new Line2D.Double(new Point2D.Double(this.cord.getX1(), this.cord.getY1()),
+							new Point2D.Double(this.cord.getX2(), this.cord.getY2() - 1));
+					Thread.sleep(this.TIME_TO_SLEEP_WHILE_EXTENDING_CORD_IN_MILLISECONDS);
+				}
+				for (int i = 0; i < 30; i++) {
+					this.cord = new Line2D.Double(new Point2D.Double(this.cord.getX1(), this.cord.getY1()),
+							new Point2D.Double(this.cord.getX2(), this.cord.getY2() + 1));
+					Thread.sleep(this.TIME_TO_SLEEP_WHILE_EXTENDING_CORD_IN_MILLISECONDS);
+				}
+			} else if (this.lastDirection == 'd') {
+				for (int i = 0; i < 30; i++) {
+					this.cord = new Line2D.Double(new Point2D.Double(this.cord.getX1(), this.cord.getY1()),
+							new Point2D.Double(this.cord.getX2(), this.cord.getY2() + 1));
+					Thread.sleep(this.TIME_TO_SLEEP_WHILE_EXTENDING_CORD_IN_MILLISECONDS);
+				}
+				for (int i = 0; i < 30; i++) {
+					this.cord = new Line2D.Double(new Point2D.Double(this.cord.getX1(), this.cord.getY1()),
+							new Point2D.Double(this.cord.getX2(), this.cord.getY2() - 1));
+					Thread.sleep(this.TIME_TO_SLEEP_WHILE_EXTENDING_CORD_IN_MILLISECONDS);
+				}
+			} else if (this.lastDirection == 'l') {
+				for (int i = 0; i < 30; i++) {
+					this.cord = new Line2D.Double(new Point2D.Double(this.cord.getX1(), this.cord.getY1()),
+							new Point2D.Double(this.cord.getX2() - 1, this.cord.getY2()));
+					Thread.sleep(this.TIME_TO_SLEEP_WHILE_EXTENDING_CORD_IN_MILLISECONDS);
+				}
+				for (int i = 0; i < 30; i++) {
+					this.cord = new Line2D.Double(new Point2D.Double(this.cord.getX1(), this.cord.getY1()),
+							new Point2D.Double(this.cord.getX2() + 1, this.cord.getY2()));
+					Thread.sleep(this.TIME_TO_SLEEP_WHILE_EXTENDING_CORD_IN_MILLISECONDS);
+				}
+			} else {
+				for (int i = 0; i < 30; i++) {
+					this.cord = new Line2D.Double(new Point2D.Double(this.cord.getX1(), this.cord.getY1()),
+							new Point2D.Double(this.cord.getX2() + 1, this.cord.getY2()));
+					Thread.sleep(this.TIME_TO_SLEEP_WHILE_EXTENDING_CORD_IN_MILLISECONDS);
+				}
+				for (int i = 0; i < 30; i++) {
+					this.cord = new Line2D.Double(new Point2D.Double(this.cord.getX1(), this.cord.getY1()),
+							new Point2D.Double(this.cord.getX2() - 1, this.cord.getY2()));
+					Thread.sleep(this.TIME_TO_SLEEP_WHILE_EXTENDING_CORD_IN_MILLISECONDS);
+				}
+			}
+
+		}
+
+		// if(this.cordExtended==false){
+		//
+		// this.cord = new Line2D.Double(new Point2D.Double(this.x, this.y),
+		// new Point2D.Double(this.x + this.changeX*10, this.y +
+		// this.changeY*10));
+		// this.cordExtended = true;
+		//
+		// }
+		// this.cord = this.DEFAULT_CORD;}
+
+		catch (InterruptedException e) {
+			//
+		}
+
+	}
 }
